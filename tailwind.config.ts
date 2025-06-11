c7c6c536830bd9d96dce7503c2baa32e2dd2f3a8
Mohammed3 <christiancamm.r.1.21.9.8.4@gmail.com>
2025-06-11 18:38:22 +0300

    Hellohh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,7 @@ const config: Config = {
       },
     },
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require('@tailwindcss/forms')],
 };
 export default config;
