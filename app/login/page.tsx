2b5ed5078c0bf905ee10c93531250ddda034171b
Mohammed3 <christiancamm.r.1.21.9.8.4@gmail.com>
2025-07-17 15:38:04 +0300

    Updated!

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -187,7 +187,7 @@ const handleSubmit = async (e: React.FormEvent) => {
                     <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                     </svg>
-                    تسجيل الدخول
+1تسجيل الدخول
                   </div>
                 )}
               </button>
