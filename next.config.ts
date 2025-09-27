a8ca48250ce6d66da984f735965c4577e4a201cc
Mohammed3 <christiancamm.r.1.21.9.8.4@gmail.com>
2025-09-27 15:25:37 +0300

    Updated

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['mahmoudmohammed.site'],
+    domains: ['aliaecommerce.com'],
   },
   async rewrites() {
     return [
       // Proxy Sanctum CSRF cookie endpoint
-      {
-        source: '/sanctum/csrf-cookie',
-        destination: 'https://mahmoudmohammed.site/sanctum/csrf-cookie',
-      },
-      // Proxy login route
-      {
-        source: '/login',
-        destination: 'https://mahmoudmohammed.site/login',
-      },
-      // Proxy all other API routes
-      {
-        source: '/api/:path*',
-        destination: 'https://mahmoudmohammed.site/api/:path*',
-      },
+
     ];
   },
 };
