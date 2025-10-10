ffaeb46e9264f3f97eb05d94c17925fed82a666b
Mohammed3 <christiancamm.r.1.21.9.8.4@gmail.com>
2025-10-10 11:25:49 +0300

    Updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['mahmoudmohammed.site'],
+    domains: ['aliaecommerce.com'],
   },
   async rewrites() {
     return [
